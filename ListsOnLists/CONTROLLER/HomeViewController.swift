db8543bb451e8c06a52af82f2fc17d256bb30db0
Erin <erin3scully@icloud.com>
2020-10-30 04:53:24 -0600

    Had to adjust code complexity in order to use UserDefaults for data persistence
    Because I was working with an array of custom objects, this is hard to store in UserDefaults. It's better to persist the data in file system once you're saving more than just small bits of very basic data

--- ListsOnLists/CONTROLLER/HomeViewController.swift
+++ ListsOnLists/CONTROLLER/HomeViewController.swift
@@ -3,50 +3,55 @@ import UIKit
 
 class HomeViewController: UITableViewController {
     
-
-      var itemListArray = [ListItem]()
-   
+//    var itemListArray = [ListItem]()
+ //   var itemListArray = [ListItem(title: "Make fruit smoothie", isComplete: false), ListItem(title: "Give Phe a bath", //isComplete: false), ListItem(title: "Study videos from beginner app dev module", isComplete: true)]
+    
+    var itemListArray = ["Make a fruit smoothie", "Give Phe a bath!", "Study app dev beginner module"]
+    
+    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        itemListArray = [ListItem(title: "Make fruit smoothie", isComplete: false), ListItem(title: "Give Phe a bath", isComplete: false), ListItem(title: "Study videos from beginner app dev module", isComplete: true)]
-    
+        if let items = defaults.array(forKey: "ItemList") as? [String] {
+            itemListArray = items
+        }
       
-        
-        
-        
         
     }
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
        
-        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
-     
-        // need to create a local TEXT FIELD VARIABLE in order to broaden scope since .addTextField function comes AFTER the alert pops up. The time point at which you obtain the information about what the user has typed in the alert happens inside the ACTION code block below since this code block is where you define what happens after user presses the "Add" button when they've completed typing:
-                        var textField = UITextField()
+// need to create a local TEXT FIELD VARIABLE in order to broaden scope since .addTextField function comes AFTER the alert pops up. The time point at which you obtain the information about what the user has typed in the alert happens inside the ACTION code block below since this code block is where you define what happens after user presses the "Add" button when they've completed typing:
+        
+        var textField = UITextField()
        
-         let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            let newItem = ListItem(title: textField.text!, isComplete: false)
-            self.itemListArray.append(newItem)
+        //Create UITextField Alert:
+        
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+         
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            
+            
+           // let newItem = ListItem(title: textField.text!, isComplete: false)
+            
+            
+            self.itemListArray.append(textField.text!)
+            self.defaults.set(self.itemListArray, forKey: "ItemList")
             self.tableView.reloadData()
         }
+        
         
         alert.addTextField { (alertTextField) in
            textField = alertTextField
             alertTextField.placeholder = "Create new item"
+          
         }
-        
         
         alert.addAction(action)
-    
-        
         present(alert, animated: true, completion: nil)
         
-       
-        }
-      
-
+       }
     
 //MARK: - Table View Methods
     
@@ -56,14 +61,13 @@ override func tableView(_ tableView: UITableView, numberOfRowsInSection section:
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
-        cell.textLabel?.text = itemListArray[indexPath.row].title
-        
+       // cell.textLabel?.text = itemListArray[indexPath.row].title
+        cell.textLabel?.text = itemListArray[indexPath.row]
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
       
-  
        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
