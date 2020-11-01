7a7e4c97420fbafa4c97ed764cddb6adae4f2eb3
Erin <erin3scully@icloud.com>
2020-11-01 03:33:08 -0700

    Work In Progress: Create CategoryViewController and Category Entity
    Current bug at time of this commit: Refactored code and created a Data Manager Singleton that handles all of the Core Data methods. Currently the app is functional; it doesn't crash. It is also successfully saving data to the persistent container, but I need to troubleshoot why the data isn't loading when app is relaunched.

--- ListsOnLists/CONTROLLERS/HomeViewController.swift
+++ ListsOnLists/CONTROLLERS/HomeViewController.swift
@@ -6,14 +6,21 @@ class HomeViewController: UITableViewController {
     
 @IBOutlet weak var searchBar: UISearchBar!
     
-var itemListArray = [ListItem]()
+//var itemListArray = [ListItem]()
 
+    var itemListArray = DataModelManager.shared.items
+    
+    var dataManager = DataModelManager.shared
+   
 let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 override func viewDidLoad() {
     super.viewDidLoad()
  
-    loadData()
+    dataManager.loadListItems()
+    tableView.reloadData()
+    
+ //   loadData()
     
     //MARK: - Set search bar as first responder in view did load so that the search text field is focused and has a blinking cursor
     searchBar.becomeFirstResponder()
@@ -25,11 +32,16 @@ override func viewDidLoad() {
             let action = UIAlertAction(title: "Add", style: .default) { (action) in
                 self.tableView.reloadData()
             
-                let newItem = ListItem(context: self.context)
+                let newItem = ListItem(context: self.dataManager.context)
+//                let newItem = ListItem(context: self.context)
                 newItem.title = textField.text!
                 newItem.isComplete = false
                 self.itemListArray.append(newItem)
-                self.saveData()
+               
+                
+                self.dataManager.saveData()
+                self.tableView.reloadData()
+                //self.saveData()
             }
         
         alert.addTextField { (alertTextField) in
@@ -42,30 +54,30 @@ override func viewDidLoad() {
         present(alert, animated: true, completion: nil)
         
     }
-//MARK: - Core Data create and read methods:
-    func saveData(){
-        do {
-        try context.save()
-        }
-        catch {
-        print("Error saving context: \(error)")
-    }
-        self.tableView.reloadData()
-    }
-    
-
-                                                            //parameter's default value:
-    func loadData(with request: NSFetchRequest<ListItem> = ListItem.fetchRequest()){
-        
-
-        do {
-          itemListArray = try context.fetch(request)
-        }
-        catch {
-            print("Fetch request error: \(error)")
-        }
-        tableView.reloadData()
-    }
+////MARK: - Core Data create and read methods:
+//    func saveData(){
+//        do {
+//        try context.save()
+//        }
+//        catch {
+//        print("Error saving context: \(error)")
+//    }
+//        self.tableView.reloadData()
+//    }
+//
+//
+//                                                            //parameter's default value:
+//    func loadData(with request: NSFetchRequest<ListItem> = ListItem.fetchRequest()){
+//
+//
+//        do {
+//          itemListArray = try context.fetch(request)
+//        }
+//        catch {
+//            print("Fetch request error: \(error)")
+//        }
+//        tableView.reloadData()
+//    }
 }
 //MARK: - EXTENSION FOR TABLE VIEW DELEGATE AND DATA SOURCE METHODS:
 
@@ -91,7 +103,9 @@ override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexP
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
      
         itemListArray[indexPath.row].isComplete.toggle()
-        saveData()
+        dataManager.saveData()
+        
+       // saveData()
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -109,21 +123,21 @@ extension HomeViewController: UISearchBarDelegate {
         request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
 
         request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: false)]
-
-        loadData(with: request)
+        dataManager.loadListItems(using: request)
+        tableView.reloadData()
+        
+//        loadData(with: request)
     }
     
   
     //MARK: - This search bar delegate method acts as a listener/observer of all action that occurs in the search bar field and is triggered every time a change is made in real-time. You can isolate particular time points as needed by your app inside this method.
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-      
-        //This line of code is saying that when the "character"/letter count in the search textfield is zero then the table view list should reload and list all items again instead of the shortened list from the user's old search query.
-        
-        //NOTE that the PLACEHOLDER TEXT count is separate from the search bar's "TEXT" count. The code below will work as it should even though technically the search bar appears to have content in it still due to the placeholder text. if there's technically "text" in the search bar because it is seaparate from PLACEHOLDER TEXT.
+ 
         if searchBar.text?.count == 0 {
-            loadData()
-            
-            // To dismiss the keyboard and the search bar text field's blinking cursor you need to resign the search bar as the first responder.    IMPORTANT NOTE --> Whenever your app is actively in session, it is good practice to grab a reference to the main view controller whenever you are writing code that directly effects the UI appearance and properties to ensure that You should grab a reference to the main view controller anytime you're writing code that effects the UI view while app is actively in session
+            dataManager.loadListItems()
+            tableView.reloadData()
+            //loadData()
+
             
             DispatchQueue.main.async {
                 searchBar.resignFirstResponder()
